1a217f47e3289af066b988d386146061144ab635
Jevgeni Glazunov <glazunov@Jevgenis-MacBook-Pro.local>
2020-06-24 12:07:17 +0200

    JS and CSS files moved inside JS and CSS folders

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
-const server = require("http").createServer(app)
+const server = require('http').createServer(app)
 const io = require('socket.io')(server);
 const port = process.env.PORT || 3000
 
-app.use(express.static(__dirname + "/public"))
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
