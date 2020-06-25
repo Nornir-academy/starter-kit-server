9ca6b2b652fb2473716d447c544752d46f9f8a63
Jevgeni Glazunov <glazunov@Jevgenis-MacBook-Pro.local>
2020-06-25 11:29:38 +0200

    On/Off buttons change classes on click

--- public/js/client.js
+++ public/js/client.js
@@ -3,6 +3,8 @@ const socket = io.connect();
 const messageElement = document.getElementById("hello-world");
 const batteryNumberElement = document.getElementById("battery-number");
 const batteryProgressElement = document.getElementById("battery-progress");
+const lightOn = document.getElementById("lightOn");
+const lightOff = document.getElementById("lightOff");
 
 socket.on('message', (data) => {
   const message = data.RTW.STRING;
@@ -23,3 +25,21 @@ function batteryLife(battery) {
 function batteryPercent(battery) {
   return battery / 3.7 * 100;
 }
+
+lightOff.addEventListener('click', function() {
+  lightOn.parentElement.classList.remove('btn-info', 'active');
+  lightOn.parentElement.classList.add('btn-outline-info');
+  this.parentElement.classList.remove('btn-outline-info');
+  this.parentElement.classList.add('btn-info', 'active');
+  lightOn.removeAttribute('checked', '');
+  lightOff.setAttribute('checked', '');
+});
+
+lightOn.addEventListener('click', function() {
+  lightOff.parentElement.classList.remove('btn-info', 'active');
+  lightOff.parentElement.classList.add('btn-outline-info');
+  this.parentElement.classList.remove('btn-outline-info');
+  this.parentElement.classList.add('btn-info', 'active');
+  lightOff.removeAttribute('checked', '');
+  lightOn.setAttribute('checked', '');
+});
